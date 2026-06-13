659ec33abbc6e1177ef42da918fb1b0bb2781035
Murilo Ribeiro <muriloribeiro11@gmail.com>
2026-06-13 13:57:51 -0300

    chore: remover inserções de jogos de exemplo do seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -5,8 +5,3 @@ INSERT INTO users (username, password_hash, is_admin, pontos_principais, pontos_
 VALUES ('admin', '$2a$08$FPsMQ6gIP9bGjBizw2daD.y0gDSfdJ3iy7z9A3Q2TDMWuEld56/HW', 1, 0, 1000);
 
 -- Jogos de exemplo da Copa. data_jogo e o prazo: depois dela trava palpite e aposta.
-INSERT INTO games (time_a, time_b, data_jogo, created_by) VALUES
-  ('Brasil', 'Argentina',  '2026-06-20 16:00:00', 1),
-  ('Franca', 'Inglaterra', '2026-06-21 13:00:00', 1),
-  ('Alemanha', 'Espanha',  '2026-06-22 16:00:00', 1),
-  ('Portugal', 'Holanda',  '2026-06-23 13:00:00', 1);
